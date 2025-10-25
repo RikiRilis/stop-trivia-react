84c1b18326370397b1eb471cef6bdc85d55ef42f
RikiRilis <thewhitzip@gmail.com>
2025-10-25 01:45:37 -0400

    Images moved to icons folder so the prebuild can generate the mipmap folders automatically

--- app.config.js
+++ app.config.js
@@ -4,11 +4,12 @@ export default {
     slug: "stop-trivia",
     version: "2.3.3",
     orientation: "portrait",
-    icon: "./assets/ic_brand.png",
+    icon: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     scheme: "stoptrivia",
     splash: {
+      image: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
       resizeMode: "contain",
       backgroundColor: "#000B0A",
     },
@@ -19,59 +20,74 @@ export default {
       googleServicesFile: "./GoogleService-Info.plist",
     },
     android: {
-        adaptiveIcon: {
-          foregroundImage: "./assets/ic_brand.png",
-          backgroundColor: "#000B0A",
-        },
-        edgeToEdgeEnabled: true,
-        predictiveBackGestureEnabled: false,
-        package: "com.rilisentertainment.stoptriviaonline",
-        googleServicesFile: "./google-services.json",
-        versionCode: 10,
-        version: "2.3.3",
-        minSdkVersion: 24,
-        ndkVersion: "29.0.14206865"
+      adaptiveIcon: {
+        foregroundImage: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
+        backgroundColor: "#000B0A",
+      },
+      icon: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.rilisentertainment.stoptriviaonline",
+      googleServicesFile: "./google-services.json",
+      versionCode: 10,
+      version: "2.3.3",
+      minSdkVersion: 24,
+      ndkVersion: "29.0.14206865",
+      softwareKeyboardLayoutMode: "pan",
     },
     web: {
-        favicon: "./assets/ic_brand.png",
+      favicon: "./assets/icons/ic_brand.png",
     },
     plugins: [
-        "@react-native-firebase/app",
-        "expo-router",
-        "expo-font",
-        [
-          "expo-image-picker",
-          {
-            photosPermission:
-              "The game accesses your photos to let you pick an image for your profile picture.",
-          },
-        ],
-        [
-          "expo-splash-screen",
-          {
+      "@react-native-firebase/app",
+      "expo-router",
+      "expo-font",
+      "@react-native-google-signin/google-signin",
+      [
+        "expo-image-picker",
+        {
+          photosPermission:
+            "The game accesses your photos to let you pick an image for your profile picture.",
+        },
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
+          backgroundColor: "#000B0A",
+          dark: {
+            image: "./assets/icons/mipmap-xxxhdpi/ic_launcher.png",
             backgroundColor: "#000B0A",
           },
-        ],
-        [
-          "expo-build-properties",
-          {
-            ios: {
-              useFrameworks: "static",
-            },
-            android: {
-              targetSdkVersion: 36,
-              compileSdk: 36,
-              compileSdkVersion: 36,
-              ndkVersion: "29.0.14206865"
-            },
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
           },
-        ],
+          android: {
+            targetSdkVersion: 36,
+            compileSdk: 36,
+            compileSdkVersion: 36,
+            ndkVersion: "29.0.14206865",
+          },
+        },
+      ],
+      [
+        "react-native-google-mobile-ads",
+        {
+          androidAppId: "ca-app-pub-5333671658707378~9974389837",
+          iosAppId: "ca-app-pub-5333671658707378~7006565543",
+        },
+      ],
     ],
     extra: {
-        router: {},
-        eas: {
-          projectId: "efcde181-f82b-437a-910b-94b2e117ac25",
-        },
+      router: {},
+      eas: {
+        projectId: "efcde181-f82b-437a-910b-94b2e117ac25",
+      },
     },
   },
 }
